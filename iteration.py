78a9e6508a04e156f54ec6da09cc7c13c018eab4
David <dagaydevel@free.fr>
2019-04-03 11:33:48 +0200

    exo itérations

---
+++ iteration.py
@@ -0,0 +1,9 @@
+
+    
+somme = 0
+for i in range(1,11):
+    somme +=  i
+
+print(somme)
+
+
---
+++ moyenne.py
@@ -0,0 +1,35 @@
+# Calcul de la moyenne
+
+def demander_nombres():
+    nombres = []
+    while True:
+        nombre_saisie = input("Saisissez un nombre : ")
+        if nombre_saisie == "":
+            break
+        nombres.append(float(nombre_saisie))
+
+    return nombres
+
+
+def moyenne(nombres):
+    return sum(nombres) / len(nombres)
+
+
+nombres_saisis = demander_nombres()
+if len(nombres_saisis) == 0:
+    print("Pas de nombre, pas de calcul de moyenne !")
+else:
+    mean = moyenne(nombres_saisis)
+    print(nombres_saisis)
+    print(f"La moyenne est {mean:.2f}")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
