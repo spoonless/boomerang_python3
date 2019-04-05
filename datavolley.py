32882d976b66c34564a1fe22791df074c3d2898e
David <dagaydevel@free.fr>
2019-04-05 13:50:38 +0200

    vendredi matin

---
+++ datavolley.py
@@ -0,0 +1,4 @@
+import scrapping
+
+scrapping.go_and_find(url="http://datavolley.lnv.fr/2117/EScoresheet/Men/", extensions=(".pdf"))
+
---
+++ scrapping.py
@@ -0,0 +1,45 @@
+import urllib.request
+import urllib.parse
+from bs4 import BeautifulSoup
+
+def create_ssl_context():
+    """désactive le contrôle SSL (utile pour Mac)"""
+    import ssl
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+
+
+def extract_filename(url):
+    return url.rsplit("/", maxsplit=1)[-1]
+
+
+def download(url):
+    filename = extract_filename(url)
+    print(f"downloading {filename}...")
+    try:
+        with urllib.request.urlopen(url, context=create_ssl_context()) as r, open(filename, mode="wb") as f:
+            for ligne in r:
+                f.write(ligne)
+        print(f"{filename} downloaded!")
+    except ValueError:
+        print(f"Sorry, could not download {url}")
+    except IOError:
+        print(f"Sorry, could not save {filename}")
+
+
+def go_and_find(url, extensions):    
+    with urllib.request.urlopen(url=url, context=create_ssl_context()) as r:
+        page = BeautifulSoup(r, features="html.parser")
+        for balise_a in page.find_all('a'):
+            lien = balise_a["href"]
+            for extension in extensions:
+                if lien.endswith(extension):
+                    lien_absolu = urllib.parse.urljoin(base=url, url=lien)
+                    download(lien_absolu)
+                    break
+
+
+if __name__ == "__main__":
+    adresse_site = "https://spoonless.github.io/boomerang_python3/index.html"
+    go_and_find(adresse_site, extensions=('.html', '.zip'))
